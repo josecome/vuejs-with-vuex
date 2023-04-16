a2e9798403976dcb08155c46ccec0007f718245a
Jose Jaime Come <josejaimecome@gmail.com>
2023-04-16 11:22:08 +0200

    Template elements added

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,79 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <div class="center" style="width: 800px">    
+<h1 style="text-align: center;">Vuejs with Vuex<br />(Task management)</h1><br />
+<div class="center" style="width: 100%;">
+    <div class="center" style="display: flex;">
+        <input type="text" id="txtaddTask"  class="form-control" v-model="taks_input" 
+        placeholder="Add task"
+        style="width: 180px;" />
+        <button @click="addtask" class="btn btn-primary" 
+        style="margin-left: 10px; background-color: #0FEFEF;"
+        >Add Task</button>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="center">
+        Total tasks: {{ totalTasks }}<br />
+        <div style="margin-bottom: 8px;">
+            <button @click="getData" class="btn btn-primary" 
+            style="padding-left: 4px; margin-right: 4px;">
+                All tasks
+        </button>
+        <button @click="TasksList('Ongoing')" class="btn btn-primary" 
+            style="padding-left: 4px; margin-right: 4px;">
+                Ongoing
+        </button>
+        <button @click="TasksList('Completed')" class="btn btn-primary" 
+            style="padding-left: 4px; margin-right: 4px;">
+                Completed
+        </button>
+        </div>
+       
+<div class="alltasks">
+    <div class="tasks" v-for="task in tasks" :key="id" style="padding: 8px; width: 80%; border: 1px solid gray;" >
+        <table>
+            <tr>
+                <td><div id="iddiv">{{ task.id }}</div></td>
+                <td>{{ task.task }}</td>
+                <td><i v-show="task.status === 'Completed'" class="bi bi-check-circle-fill" style="font-size: 24px; color: #EC7063;"></i></td>
+                <td>
+                    <button v-show="task.status === 'Ongoing'" @click="completed(task.id)">Mark as completed</button>
+                </td>
+            </tr>
+        </table>      
+    </div> 
+</div>
+    </div>
+</div>
+</div>
 </template>
+<script>
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Home
+  },
+  data() {
+    return {
+      tasks: [],
+      status: 'Ongoing',
+      totalTasks: 0,
+    }
+  },
+  methods: {
+    totalTasks: function () {
 
+    },
+    TasksList: function (v) {
+
+    },
+  },
+  computed: {
+    
+  }
+}
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
